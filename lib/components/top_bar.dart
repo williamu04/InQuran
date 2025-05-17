3602ec4f8a8cde8b2c2df1494db09e89bc3c9af1
NaufalH27 <naufalhmuzakki@gmail.com>
2025-05-17 11:34:36 +0700

    add dynamic padding and fix coloring

--- lib/components/top_bar.dart
+++ lib/components/top_bar.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-PreferredSizeWidget topBar(BuildContext context) {
+PreferredSizeWidget topBar(BuildContext context, GoRouterState state) {
+  final  currentRoute = state.uri.toString();
+
   return AppBar(
-    backgroundColor: Colors.deepPurpleAccent,
+    backgroundColor: Colors.transparent,
     elevation: 0,
     leading: Builder(
       builder:
           (context) => IconButton(
-            icon: Icon(Icons.menu, color: Colors.white),
+            icon: Icon(
+            Icons.menu,
+            color: currentRoute == "/book" ? Color(0xFF7C8BA0): Colors.white 
+            ),
             onPressed: () {
               Scaffold.of(context).openDrawer();
             },
@@ -16,14 +22,18 @@ PreferredSizeWidget topBar(BuildContext context) {
     title: Text(
       'QuranApp',
       style: TextStyle(
-        color: Colors.white,
+        fontFamily: "Plus Jakarta",
+        color: currentRoute == "/book" ? Color(0xFF672CBC) : Colors.white,
         fontWeight: FontWeight.bold,
         fontSize: 18,
       ),
     ),
     actions: [
       IconButton(
-        icon: Icon(Icons.settings, color: Colors.white),
+        icon: Icon(
+          Icons.settings, 
+          color: currentRoute == "/book" ? Color(0xFF7C8BA0): Colors.white
+        ),
         onPressed: () {
           // Tpengaturan
         },
