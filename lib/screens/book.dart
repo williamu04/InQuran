9d4b22d89e3f4c384724401e7fb1f9c6f0c317fc
NaufalH27 <naufalhmuzakki@gmail.com>
2025-05-17 08:54:10 +0700

    add initial structure for book screens

--- lib/screens/book.dart
+++ lib/screens/book.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+enum SurahViewMode {
+  surah, juz
+}
 class BookScreen extends StatelessWidget {
   const BookScreen({super.key});
 
@@ -10,12 +13,99 @@ class BookScreen extends StatelessWidget {
       height: double.infinity,
       child : Column(
         children: [
-          Center(
-            child: Text("Book")
-          )
+          searchBox(),
+          SurahWidget()
         ],
 
       ),
+    );
+  }
+}
+
+Widget searchBox() {
+  return Container(
+
+  );
+
+}
+
+class SurahWidget extends StatefulWidget {
+  const SurahWidget({super.key});
+
+  @override
+  _SurahWidgetState createState() => _SurahWidgetState();
+}
+
+class _SurahWidgetState extends State<SurahWidget> {
+
+  SurahViewMode currentMode = SurahViewMode.surah;  // Surah as Default
+
+  void _switchToSurahMode() {
+    setState(() {
+      currentMode = SurahViewMode.surah;
+    });
+  }
+
+  void _switchToJuzMode() {
+    setState(() {
+      currentMode = SurahViewMode.juz;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        surahNavContainer(),
+        surahContainer()
+      ],
+    );
+  }
+
+  Widget surahNavContainer() {
+    return Row(
+      children: [
+        Expanded(
+          child: TextButton(
+            onPressed: () {
+              _switchToSurahMode();
+            },
+            child: Text('Surah'),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            onPressed: () {
+              _switchToJuzMode();
+            },
+            child: Text('Juz'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget surahContainer() {
+    if (currentMode == SurahViewMode.surah) {
+      return surahViewMode();
+    } else {
+      return juzViewMode();
+    }
+  }
+
+  Widget surahViewMode() {
+    return Column(
+      children: [
+        Text('Surah View'),
+      ],
+    );
+  }
+
+  Widget juzViewMode() {
+    return Column(
+      children: [
+        Text('Juz View'),
+      ],
     );
   }
 }
