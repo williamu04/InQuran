a6c1cf24d2af8843cd1539caeb5f241703d9314c
NaufalH27 <naufalhmuzakki@gmail.com>
2025-06-01 15:56:48 +0700

    add daos

---
+++ lib/data/local/dao/ayah_dao.g.dart
@@ -0,0 +1,9 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'ayah_dao.dart';
+
+// ignore_for_file: type=lint
+mixin _$AyahDaoMixin on DatabaseAccessor<AppDatabase> {
+  $SurahTable get surah => attachedDatabase.surah;
+  $AyahTable get ayah => attachedDatabase.ayah;
+}
---
+++ lib/data/local/dao/surah_dao.g.dart
@@ -0,0 +1,8 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'surah_dao.dart';
+
+// ignore_for_file: type=lint
+mixin _$SurahDaoMixin on DatabaseAccessor<AppDatabase> {
+  $SurahTable get surah => attachedDatabase.surah;
+}
