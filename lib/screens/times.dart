4c11a4145f5616871cdecc2150eef21408760ffe
williamu04 <williamputra04@gmail.com>
2025-09-03 10:54:03 +0700

    major update prayer time page

--- lib/screens/times.dart
+++ lib/screens/times.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:mtqmnuns/models/prayer.dart';
+import 'package:mtqmnuns/screens/login.dart';
 import 'package:mtqmnuns/services/geocode.dart'; // Change this line
 import 'package:mtqmnuns/services/prayer.dart';
 
@@ -73,66 +75,97 @@ class _PrayerScreenState extends State<PrayerTimeScreen> {
               ? const Center(child: CircularProgressIndicator())
               : prayerTime == null
               ? const Center(child: Text("Gagal memuat data"))
-              : Column(
-                // padding: const EdgeInsets.all(16),
-                children: [
-                  // Box lokasi
-                  Container(
-                    padding: const EdgeInsets.all(12),
-                    margin: const EdgeInsets.only(bottom: 16),
-                    decoration: BoxDecoration(
-                      color: Colors.blue.shade50,
-                      borderRadius: BorderRadius.circular(12),
-                      border: Border.all(color: Colors.blueAccent),
-                    ),
-                    child: Row(
-                      children: [
-                        const Icon(Icons.location_on, color: Colors.blueAccent),
-                        const SizedBox(width: 8),
-                        Expanded(
-                          child: Text(
-                            currentLocation,
-                            style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500,
+              : Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    // Box lokasi
+                    Container(
+                      padding: const EdgeInsets.all(12),
+                      margin: const EdgeInsets.only(bottom: 16),
+                      decoration: BoxDecoration(
+                        color: Colors.blue.shade50,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Row(
+                        children: [
+                          const Icon(
+                            Icons.location_on,
+                            color: Color(0xff672CBC),
+                          ),
+                          const SizedBox(width: 8),
+                          Expanded(
+                            child: Text(
+                              currentLocation,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                              overflow: TextOverflow.ellipsis,
                             ),
                           ),
+                        ],
+                      ),
+                    ),
+
+                    // Add TODAY and date
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "TODAY",
+                          style: TextStyle(
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xff3B1D77),
+                            letterSpacing: 2,
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        Text(
+                          DateFormat('d MMMM yyyy').format(DateTime.now()),
+                          style: const TextStyle(
+                            fontSize: 18,
+                            color: Color(0xff3B1D77),
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                      ],
+                    ),
+
+                    // daftar jadwal sholat
+                    Column(
+                      children: [
+                        PrayerCard(
+                          title: "Fajr",
+                          time: prayerTime!.fajr,
+                          icon: Icons.wb_twilight_rounded,
+                        ),
+                        PrayerCard(
+                          title: "Dzuhr",
+                          time: prayerTime!.dhuhr,
+                          icon: Icons.wb_sunny,
+                        ),
+                        PrayerCard(
+                          title: "Asr",
+                          time: prayerTime!.asr,
+                          icon: Icons.wb_sunny_outlined,
+                        ),
+                        PrayerCard(
+                          title: "Maghrib",
+                          time: prayerTime!.maghrib,
+                          icon: Icons.nightlight_outlined,
+                        ),
+                        PrayerCard(
+                          title: "Isha",
+                          time: prayerTime!.isha,
+                          icon: Icons.nightlight,
                         ),
                       ],
                     ),
-                  ),
-
-                  // daftar jadwal sholat
-                  Column(
-                    children: [
-                      PrayerCard(
-                        title: "Subuh",
-                        time: prayerTime!.fajr,
-                        icon: Icons.wb_twilight_rounded,
-                      ),
-                      PrayerCard(
-                        title: "Dzuhur",
-                        time: prayerTime!.dhuhr,
-                        icon: Icons.wb_sunny,
-                      ),
-                      PrayerCard(
-                        title: "Ashar",
-                        time: prayerTime!.asr,
-                        icon: Icons.wb_sunny_outlined,
-                      ),
-                      PrayerCard(
-                        title: "Maghrib",
-                        time: prayerTime!.maghrib,
-                        icon: Icons.nightlight_outlined,
-                      ),
-                      PrayerCard(
-                        title: "Isya",
-                        time: prayerTime!.isha,
-                        icon: Icons.nightlight,
-                      ),
-                    ],
-                  ),
-                ],
+                  ],
+                ),
               ),
     );
   }
@@ -156,19 +189,42 @@ class PrayerCard extends StatelessWidget {
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
       elevation: 2,
       margin: const EdgeInsets.symmetric(vertical: 6),
+      color: Color(0xff672CBC),
       child: ListTile(
-        leading: Icon(icon, color: Colors.blueAccent, size: 28),
+        leading: Icon(icon, color: Color(0xff994EF8), size: 28),
         title: Text(
           title,
-          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          style: const TextStyle(
+            fontWeight: FontWeight.w900,
+            fontSize: 18,
+            color: Color(0xffF5F9FE),
+          ),
         ),
         trailing: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Text(time, style: const TextStyle(fontSize: 16)),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 6),
+              decoration: BoxDecoration(
+                color: const Color(0xff3B1D77),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              width: 100,
+              height: 40,
+              alignment: Alignment.center,
+
+              child: Text(
+                time,
+                style: const TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                  // fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
             const SizedBox(width: 12),
             IconButton(
-              icon: const Icon(Icons.alarm, color: Colors.grey),
+              icon: const Icon(Icons.alarm_add, color: Colors.white),
               onPressed: () {
                 // TODO: aksi untuk set alarm
                 ScaffoldMessenger.of(context).showSnackBar(
